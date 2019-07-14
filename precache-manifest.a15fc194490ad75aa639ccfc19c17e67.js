c3e689757d57bb6f86724930013386b0e60d1d58
Emmanuel Adegor <eiadegor@unionbankng.com>
2019-07-14 19:34:17 +0100

    Updates

---
+++ precache-manifest.a15fc194490ad75aa639ccfc19c17e67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fb64c5f937efa99d7eda086598a3cb7",
+    "url": "/Emmanueladegor/Lyrics-finder/index.html"
+  },
+  {
+    "revision": "db5f27301dbed499a60f",
+    "url": "/Emmanueladegor/Lyrics-finder/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "96f439316f88c2bb451b",
+    "url": "/Emmanueladegor/Lyrics-finder/static/js/2.4c7503a7.chunk.js"
+  },
+  {
+    "revision": "db5f27301dbed499a60f",
+    "url": "/Emmanueladegor/Lyrics-finder/static/js/main.dbd21430.chunk.js"
+  },
+  {
+    "revision": "6fdcbfcfa3e6382a7576",
+    "url": "/Emmanueladegor/Lyrics-finder/static/js/runtime~main.2d748ea9.js"
+  }
+]);
